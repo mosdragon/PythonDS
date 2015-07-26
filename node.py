521c41984ed86b227b25db45817b336501d31c1b
Osama Sakhi <msakhi21@gmail.com>
2015-07-26 13:06:56 -0400

    Fixed node class

--- node.py
+++ node.py
@@ -1,48 +1,59 @@
 
 class Node:
-	'''This is a node class that can hold a left child and a right child'''
+    '''This is a node class that can hold a left child and a right child'''
 
-	def __init__(self, data):
-		self.left = None
-		self.right = None
-		self.data = data
-		self.height = 0
-
-
-	def determineheight(self):
-		if (self.left):
-			left_height = self.left.getheight()
-			if left_height + 1 > self.getheight():
-				self.height = left_height + 1
-
-		if (self.right):
-			right_height = self.right.getheight()
-			if right_height + 1 > self.getheight():
-				self.height = right_height + 1
-
-		if (not (self.right and self.left)):
-			self.height = 0
-
-	def setleft(self, left):
-		self.left = left
-		self.determineheight()
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+        self.height = 0
 
 
-	def getleft(self):
-		return self.left
+    def determineheight(self):
 
-	def setright(self, right):
-		self.right = right
-		self.determineheight()
+        left_height = self.getleft().getheight() if self.getleft() else -1
+        right_height = self.getright().getheight() if self.getright() else -1
+        self.height = max(left_height, right_height) + 1
 
-	def getright(self):
-		return self.right
+    def setleft(self, left):
+        self.left = left
+        self.determineheight()
 
-	def getheight(self):
-		return self.height
 
-	def setdata(self, data):
-		self.data = data
+    def getleft(self):
+        return self.left
 
-	def getdata(self):
-		return self.data
+    def setright(self, right):
+        self.right = right
+        self.determineheight()
+
+    def getright(self):
+        return self.right
+
+    def getheight(self):
+        return self.height
+
+    def setdata(self, data):
+        self.data = data
+
+    def getdata(self):
+        return self.data
+
+    def __eq__(self, other):
+        '''Equality of nodes only depends on the data and the height of the nodes'''
+
+        if isinstance(other, self.__class__):
+            match = (self.getdata() == other.getdata()
+                and self.getheight() == other.getheight())
+            return match
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        template = "DATA: {}\t" + "HEIGHT: {}"
+        return str.format(template, self.data, self.height)
+
+
---
+++ test_node.py
@@ -0,0 +1,124 @@
+from node import Node
+import unittest
+
+
+class MyTest(unittest.TestCase):
+    
+    def test_init(self):
+        startdata = 100
+        root = Node(startdata)
+        self.assertEqual(root.getheight(), 0)
+        self.assertEqual(root.getdata(), startdata)
+        self.assertIsNone(root.getleft())
+        self.assertIsNone(root.getright())
+
+
+    def test_setting_leaf_nodes(self):
+        parent_data = 10
+        parent = Node(parent_data)
+
+        self.assertEqual(parent.getheight(), 0)
+
+        left_child = Node(5)
+        right_child = Node(20)
+
+        parent.setleft(left_child)
+        self.assertEqual(parent.getheight(), 1)
+
+        parent.setright(right_child)
+        self.assertEqual(parent.getheight(), 1)
+
+        parent.setleft(None)
+        self.assertEqual(parent.getheight(), 1)
+
+        parent.setright(None)
+        self.assertEqual(parent.getheight(), 0)
+
+        # Parent no longer has any children nodes
+
+        # Left child node gets two children added
+        left_gchild = Node(4)
+        right_gchild = Node(6)
+        left_child.setleft(left_gchild)
+        left_child.setright(right_gchild)
+        self.assertEqual(left_child.getheight(), 1)
+
+        # Parent gets left_child as its child
+        parent.setleft(left_child)
+        self.assertEqual(parent.getheight(), left_child.getheight() + 1)
+
+
+    def test_determineheight(self):
+        
+        left_child = Node(5)
+        self.assertEqual(left_child.getheight(), 0) 
+        
+        left_child.height = 4
+        self.assertEqual(left_child.getheight(), 4)
+
+        right_child = Node(20)
+        self.assertEqual(right_child.getheight(), 0)
+
+        parent = Node(10)
+        parent.setright(right_child)
+        self.assertEqual(parent.getheight(), 1)
+        
+        parent.setleft(left_child)
+        self.assertEqual(parent.getheight(), left_child.getheight() + 1)
+
+        left_child = None
+        parent.setleft(None)
+        self.assertEqual(parent.getheight(), right_child.getheight() + 1)
+
+        left_child = Node(5)
+        parent.setleft(left_child)
+
+        self.assertEqual(parent.getheight(), left_child.getheight() + 1)
+        self.assertEqual(parent.getheight(), right_child.getheight() + 1)
+
+
+        # These will be the children of left_child
+        left_gchild = Node(15)
+        right_gchild = Node(25)
+        left_child.setleft(left_gchild)
+        left_child.setleft(right_gchild)
+
+        self.assertEqual(left_child.getheight(), left_gchild.getheight() + 1)
+
+        # Parent height will be unchanged despite child's height changing
+        # Until the parent calls determineheight() function
+        self.assertEqual(parent.getheight(), right_child.getheight() + 1)
+        self.assertNotEqual(parent.getheight(), left_child.getheight() + 1)
+
+        # Now call determineheight() on parent. It will not have the height of
+        # left_child height plus 1
+        parent.determineheight()
+        self.assertEqual(parent.getheight(), left_child.getheight() + 1)
+        self.assertNotEqual(parent.getheight(), right_child.getheight() + 1)
+
+
+    def test_equality(self):
+        data = 10
+        original = Node(data)
+        duplicate = Node(data)
+
+        self.assertEqual(original, duplicate)
+
+        left_child = Node(5)
+
+        original.setleft(left_child)
+
+        self.assertNotEqual(original, duplicate)
+
+        duplicate.setleft(left_child)
+        self.assertEqual(original, duplicate)
+
+        # Demonstrate that nodes equal despite having different left and right children
+
+        duplicate.setleft(None)
+        right_child = left_child
+        duplicate.setright(right_child)
+        self.assertEqual(original, duplicate)
+
+
+unittest.main() 
